5efe756eee08ffe0e509ee0847e80551e15915f3
vrancxai <fraenze.kondritz@dci-student.org>
2025-08-21 10:49:42 +0200

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LightDarkMode from "./components/light-dark-mode";
+import QRCodeGenerator from "./components/qr-code";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+     
+      { <StarRating noOfStars={10}/>}
+
+      {<QRCodeGenerator/> }
+
+      {<LightDarkMode/> }
+
+     
     </div>
   );
 }
